c5408ef38e1d172cec09bd0db37bfcef4ab5f509
Srijan Srivastava <srijan@careerhackers.io>
2023-06-02 13:27:17 +0530

    Initializing code

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    console.log('Morgan Initialized')
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+module.exports = app;
