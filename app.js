e7f13b74f6f0e4f39b4c27cab4ed9ea679ca1869
Srijan Srivastava <srijan@careerhackers.io>
2023-06-02 13:36:01 +0530

    Adding prettier and eslint options

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const morgan = require('morgan');
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development') {
-    console.log('Morgan Initialized')
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  console.log('Morgan Initialized');
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 module.exports = app;
