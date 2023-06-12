43b8af66315753624f1d32fc3f6555b86fea7b1d
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-12 15:30:50 +0800

    temporary API tests (which worked)

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -18,34 +18,36 @@ exports.getAllMessage = async (req, res) => {
 };
 
 exports.addMessage = async (req, res) => {
-  try {
-    const { to, from, message, roomId } = req.body;
-    const data = await Message.create({
-      to: to,
-      from: from,
-      message: message,
-      roomId: roomId,
-    });
-    /**
-     * Connecting to Python server
-     * The answer returned can be transmitted back to the chat client.
-     * This message can then be rendered successfully.
-     */
-    if (data)
-      return res.status(200).json({
-        status: 'success',
-        data: {
-          message: 'Chat Added Successfully',
-        },
-      });
-    return res.status(400).json({
-      status: 'failed',
-      message: 'Chat was not added into the database',
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'failed',
-      message: 'Failed to retrieve messages: ' + err.message,
-    });
-  }
+  // try {
+  const { to, from, message, timestamp, roomId } = req.body;
+  const data = await Message.create({
+    to: to,
+    from: from,
+    message: message,
+    timestamp: timestamp,
+    roomId: roomId,
+  });
+  await data.save();
+  res.send(data);
+  /**
+   * Connecting to Python server
+   * The answer returned can be transmitted back to the chat client.
+   * This message can then be rendered successfully.
+   */
+  //   if (data)
+  //     return res.status(200).json({
+  //       status: 'success',
+  //       data: {
+  //         message: 'Chat Added Successfully',
+  //       },
+  //     });
+  //   return res.status(400).json({
+  //     status: 'failed',
+  //     message: 'Chat was not added into the database',
+  //   });
+  // } catch (err) {
+  //   res.status(404).json({
+  //     status: 'failed',
+  //     message: 'Failed to retrieve messages: ' + err.message,
+  //   });
 };
