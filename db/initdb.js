ed2246a040a02fa4e2f33db4f90c7e7e929bb8dc
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-12 15:32:12 +0800

    fixed naming conventions in data types and removed initial testing databases

--- db/initdb.js
+++ db/initdb.js
@@ -8,7 +8,7 @@ db.createCollection('messages', {
       bsonType: 'object',
       required: ['to', 'from', 'message', 'timestamp', 'roomId'],
       properties: {
-        to: {
+        to: { 
           bsonType: 'string',
           description: 'must be a string and is required',
         },
@@ -25,8 +25,8 @@ db.createCollection('messages', {
           description: 'must be a date and is required',
         },
         roomId: {
-          bsonType: 'objectId',
-          description: 'must be an objectId and is required',
+          bsonType: 'string',
+          description: 'must be an string and is required',
         },
       },
     },
@@ -64,33 +64,3 @@ db.createCollection('rooms', {
   },
 });
 
-// Create rooms data
-const roomData = [
-  {
-    user: "JohnDoe",
-    chatName: "Technology Discussion",
-    latestMessage: "Did you hear about the latest tech news?",
-    createdAt: new Date("2023-05-01T10:20:30Z"),
-    prompt: "Discuss the latest tech news and trends",
-  }
-];
-
-const messageData = [
-  {
-    to: 'JaneDoe',
-    from: 'JohnDoe',
-    message: 'Hello, how are you?',
-    timestamp: new Date("2023-06-10T07:34:00Z"),
-    roomId: ObjectID("60d663bb2ef4853f70fc3a4c")
-  },
-  {
-    to: 'JohnDoe',
-    from: 'JaneDoe',
-    message: 'I am good. How about you?',
-    timestamp: new Date("2023-06-10T07:36:00Z"),
-    roomId: ObjectID("60d663bb2ef4853f70fc3a4c")
-  },
-];
-
-// Insert messages data into the messages collection
-await db.messages.insertMany(messageData);
