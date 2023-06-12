3f4b5109a20a8e6b3211d9851048aa071c225764
Owen Valentinus <owenwijaya89@gmail.com>
2023-06-12 09:47:42 +0800

    also fixed variable  naming when routing

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -7,8 +7,8 @@ const {
   } = require('../controllers/roomController');
   
 // Room Routes for the frontend to create buttons that make requests to the endpoints
-router.route('/room').post(createRoom); // Create a new room
-router.route('/room/:roomId').delete(deleteRoom); // Delete a room by its ID
-router.route('/room/:roomId/leave').patch(leaveRoom); // Remove the current user from a room
+roomRouter.route('/room').post(createRoom); // Create a new room
+roomRouter.route('/room/:roomId').delete(deleteRoom); // Delete a room by its ID
+roomRouter.route('/room/:roomId/leave').patch(leaveRoom); // Remove the current user from a room
   
 module.exports = roomRouter;
