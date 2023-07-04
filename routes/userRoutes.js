5f5efbf15b379248772054a40dc430c4b28def85
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 13:35:20 +0800

    documented user endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,17 +10,56 @@ import {
 
 const userRouter = Router();
 
-// create a new user
+/**
+ * POST /api/users
+ * @summary This endpoint creates a new user
+ * @tags users
+ * @return {object} 201 - Success response - application/json
+ */
 userRouter.post('/', createUser);
-// fetch all users
+
+/**
+ * GET /api/users
+ * @summary This endpoint retrieves all users
+ * @tags users
+ * @return {object} 200 - Success response - application/json
+ */
 userRouter.get('/', getAllUsers);
-// fetch users by roomId
+
+/**
+ * GET /api/users/:roomId
+ * @summary This endpoint retrieves users by roomId
+ * @tags users
+ * @param {string} roomId.path - required
+ * @return {object} 200 - Success response - application/json
+ */
 userRouter.get('/:roomId', getUsers);
-// fetch a user by ID
+
+/**
+ * GET /api/users/:id
+ * @summary This endpoint retrieves a user by ID
+ * @tags users
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
 userRouter.get('/:id', getUserById);
-// update a user by ID
+
+/**
+ * PUT /api/users/:id
+ * @summary This endpoint updates a user by ID
+ * @tags users
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
 userRouter.put('/:id', updateUser);
-// delete a user by ID
+
+/**
+ * DELETE /api/users/:id
+ * @summary This endpoint deletes a user by ID
+ * @tags users
+ * @param {string} id.path - required
+ * @return {object} 204 - Success response - application/json
+ */
 userRouter.delete('/:id', deleteUser);
 
 export default userRouter;
