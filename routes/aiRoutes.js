3c2c6cf44d9af9b05b1283a49bf843c7d4185181
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 13:36:51 +0800

    documented ai endpoints

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -9,7 +9,41 @@ import {
 
 const airouter = Router();
 
+/**
+ * GET /api/ai
+ * @summary This endpoint retrieves all AIs
+ * @tags ai
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * POST /api/ai
+ * @summary This endpoint creates a new AI
+ * @tags ai
+ * @return {object} 201 - Success response - application/json
+ */
 airouter.route('/').get(getAllAIs).post(createAI);
+
+/**
+ * GET /api/ai/:id
+ * @summary This endpoint retrieves an AI by ID
+ * @tags ai
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * PUT /api/ai/:id
+ * @summary This endpoint updates an AI by ID
+ * @tags ai
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * DELETE /api/ai/:id
+ * @summary This endpoint deletes an AI by ID
+ * @tags ai
+ * @param {string} id.path - required
+ * @return {object} 204 - Success response - application/json
+ */
 airouter.route('/:id').get(getAIById).put(updateAI).delete(deleteAI);
 
 export default airouter;
