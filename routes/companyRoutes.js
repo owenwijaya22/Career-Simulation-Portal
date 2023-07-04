3775be123af37ce3cf939a1471177fdbe03239fa
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 13:36:25 +0800

    documented company endpoints

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-
 import {
   createCompany,
   getAllCompanies,
@@ -10,11 +9,41 @@ import {
 
 const companyRouter = Router();
 
+/**
+ * GET /api/companies
+ * @summary This endpoint retrieves all companies
+ * @tags companies
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * POST /api/companies
+ * @summary This endpoint creates a new company
+ * @tags companies
+ * @return {object} 201 - Success response - application/json
+ */
 companyRouter.route('/').get(getAllCompanies).post(createCompany);
-companyRouter
-  .route('/:id')
-  .get(getCompanyById)
-  .put(updateCompany)
-  .delete(deleteCompany);
+
+/**
+ * GET /api/companies/:id
+ * @summary This endpoint retrieves a company by ID
+ * @tags companies
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * PUT /api/companies/:id
+ * @summary This endpoint updates a company by ID
+ * @tags companies
+ * @param {string} id.path - required
+ * @return {object} 200 - Success response - application/json
+ */
+/**
+ * DELETE /api/companies/:id
+ * @summary This endpoint deletes a company by ID
+ * @tags companies
+ * @param {string} id.path - required
+ * @return {object} 204 - Success response - application/json
+ */
+companyRouter.route('/:id').get(getCompanyById).put(updateCompany).delete(deleteCompany);
 
 export default companyRouter;
