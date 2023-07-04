874fe0949015ec16b01b2612a22853a50d6240ac
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-04 13:35:56 +0800

    documented question endpoints

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -6,7 +6,20 @@ import {
 
 const questionRouter = Router();
 
+/**
+ * GET /api/questions
+ * @summary This endpoint retrieves all questions
+ * @tags questions
+ * @return {object} 200 - Success response - application/json
+ */
 questionRouter.route('/').get(getQuestions);
+
+/**
+ * POST /api/questions
+ * @summary This endpoint creates a new question
+ * @tags questions
+ * @return {object} 201 - Success response - application/json
+ */
 questionRouter.route('/').post(createQuestion);
 
 export default questionRouter;
