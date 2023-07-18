d359b61dee20b5e46570c2503c9128c0b77f3d9a
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-18 11:41:45 +0800

    updated typos

--- controllers/pythonController.js
+++ controllers/pythonController.js
@@ -2,19 +2,15 @@ const { exec } = require("child_process");
 
 export async function handlePostRequest (req, res) {
     const jsonData = req.body;
-    const formattedData = JSON.stringify(jsonData).replace(/(:)|(")/g, (match, group1, group2) => {
-        if (group1) return ': ';
-        if (group2) return '\\"';
-    });
+    const escapedJsonString = JSON.stringify(jsonString).replace(/"/g, '\\"');
 
-    exec(`python app.py '${formattedData}'`, (error, stdout, stderr) => {
+    exec(`python gpt.py "${escapedJsonString}"`, (error, stdout) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
         }
         console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
     });
-
+    
     res.send('Request received');
 }
