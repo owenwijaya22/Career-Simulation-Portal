8d74b1d7cb5fb766485ae4dab7d9be2905435f92
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-26 11:45:05 +0800

    finalized python controllers for async

--- controllers/pythonController.js
+++ controllers/pythonController.js
@@ -1,11 +1,11 @@
 import { spawn } from 'child_process';
 
-async function handlePostRequest(req, res) {
+export async function handlePostRequest1(req, res) {
   try {
     const jsonData = req.body;
     const escapedJsonString = JSON.stringify(jsonData).replace(/"/g, '\\"');
 
-    const pythonProcess = spawn('python', ['./python/gpt.py', escapedJsonString]);
+    const pythonProcess = spawn('python', ['./controllers/gpt.py', escapedJsonString]);
 
     pythonProcess.stdout.on('data', (data) => {
       res.send(data.toString());
@@ -21,4 +21,26 @@ async function handlePostRequest(req, res) {
   }
 }
 
-export default handlePostRequest;
+
+export async function handlePostRequest2(req, res) {
+  try {
+    const jsonData = req.body;
+    const escapedJsonString = JSON.stringify(jsonData).replace(/"/g, '\\"');
+
+    const pythonProcess = spawn('python', ['./controllers/gpt2.py', escapedJsonString]);
+
+    pythonProcess.stdout.on('data', (data) => {
+      res.send(data.toString());
+    });
+
+    pythonProcess.on('error', (err) => {
+      console.error('Error while executing python script:', err);
+      res.status(500).send('An error occurred while processing the request.');
+    });
+  } catch (err) {
+    console.error('Error while executing python script:', err);
+    res.status(500).send('An error occurred while processing the request.');
+  }
+}
+
+
