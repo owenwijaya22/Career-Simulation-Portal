a36340de42b1d6a2b43fb8d6a35a23198855f2cc
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2023-08-21 16:36:18 +0800

    proposal-ft

--- routes/proposalRoute.js
+++ routes/proposalRoute.js
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import {
   createProposal,
   saveProposal,
-  getProposal
+  getProposal,
+  getProposals,
+  unlockProposal,
 } from '../controllers/proposalController.js';
 
 const proposalRouter = Router();
 
-proposalRouter.get('/', getProposal).post('/', createProposal).patch('/', saveProposal);
+proposalRouter
+  .get('/getProposal/', getProposal)
+  .get('/getProposals/', getProposals)
+  .post('/', createProposal)
+  .patch('/', saveProposal);
+
+proposalRouter.put('/:id', unlockProposal);
 
 export default proposalRouter;
